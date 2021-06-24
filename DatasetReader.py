11ec6020ef6e7b273181d95586e204b1f8d5cf25
wangchenghan <869723801@qq.com>
2021-06-24 16:17:53 +0800

    feature:tested datasetReader

--- DatasetReader.py
+++ DatasetReader.py
@@ -1,3 +1,4 @@
+# -- coding:utf-8 --
 import os
 import glob
 import numpy as np
@@ -19,8 +20,8 @@ class DatasetReader:
                 image_name = os.path.basename(image_path).split('.')[0]
                 label_name = image_name + '.npy'
                 density_path = os.path.join(self.density_path, label_name)
-                density_path = density_path if os.path.exsist(density_path) else ''
-                self.file_list.append({"image_path": image_path, "density_path": density_path})
+                density_path = density_path if os.path.exists(density_path) else ''
+                self.file_path_list.append((image_path, density_path))
 
     def generate_image_and_density(self, size=None):
         for image_path, density_map in self.file_path_list:
@@ -28,17 +29,17 @@ class DatasetReader:
             density_map = np.load(density_map) if density_map else None
             if size and len(size) == 2:
                 image = img_as_ubyte(resize(image, size))
-                if density_map:
+                if not(density_map is None):
                     density_map = desity_map_resize_v3(density_map, size)
+            if not (density_map is None) and (density_map.shape[0] != image.shape[0] or density_map.shape[1] != image.shape[1]):
+                raise Exception("图像的尺寸和密度图的尺寸不匹配，请重新生成，或在generate_slice_and_labels方法中指定一个新的尺寸（size）")
             yield image, density_map
 
     def generate_slice_and_labels(self, window_size=31, batch_size=4, size=None):
-        image_batch = np.zeros((batch_size, window_size, window_size, image.shape[2]), np.uint8)
-        ground_truth_batch = np.zeros((batch_size, np.float))
+        image_batch = np.zeros((batch_size, window_size, window_size, 3), np.uint8)
+        ground_truth_batch = np.zeros((batch_size), np.float)
         batch_index = 0
         for image, density_map in self.generate_image_and_density(size):
-            if density_map and (density_map.shape[0] != image.shape[0] or density_map.shape[1] != image.shape[1]):
-                raise Exception("图像的尺寸和密度图的尺寸不匹配，请重新生成，或在generate_slice_and_labels方法中指定一个新的尺寸（size）")
             if window_size % 2 == 0:
                 raise Exception("窗口大小应该是奇数")
             pad_size = window_size // 2
@@ -47,25 +48,47 @@ class DatasetReader:
                 padded_image = np.pad(image, ((pad_size, pad_size), (pad_size, pad_size)), 'constant')
             else:
                 padded_image = np.pad(image, ((pad_size, pad_size), (pad_size, pad_size), (0, 0)), 'constant')
-            if density_map: 
+            if not (density_map is None): 
                 padded_density_map = np.pad(density_map, ((pad_size, pad_size), (pad_size, pad_size)), 'constant')
                 for i in range(image.shape[0]):
                     for j in range(image.shape[1]):
                         cropped_image = np.array(padded_image[i: i + window_size, j: j + window_size])
                         ground_truth_value = padded_density_map[(2 * i + window_size) // 2, (2 * j + window_size) // 2]
-                        image_batch[batch_index] = cropped_image
+                        image_batch[batch_index,:,:,:] = cropped_image
                         ground_truth_batch[batch_index] = ground_truth_value
                         batch_index += 1
                         if batch_index >= batch_size:
                             yield image_batch, ground_truth_batch
+                            image_batch = np.zeros((batch_size, window_size, window_size, 3), np.uint8)
+                            ground_truth_batch = np.zeros((batch_size), np.float)
                             batch_index = 0
             else: 
                 for i in range(image.shape[0]):
                     for j in range(image.shape[1]):
                         cropped_image = np.array(padded_image[i: i + window_size, j: j + window_size])
-                        image_batch[batch_index] = cropped_image
+                        image_batch[batch_index,:,:,:] = cropped_image
                         ground_truth_batch[batch_index] = -1
                         batch_index += 1
                         if batch_index >= batch_size:
                             yield image_batch, ground_truth_batch
+                            image_batch = np.zeros((batch_size, window_size, window_size, 3), np.uint8)
+                            ground_truth_batch = np.zeros((batch_size), np.float)
                             batch_index = 0
+
+if __name__ == '__main__':
+    image_path = "/Users/Biomind/Documents/周文静/TasselNet/spruce/202011/train"
+    density_path = "/Users/Biomind/Documents/周文静/TasselNet/spruce/202011_den/train"
+    size = (224, 224)
+    date_reader = DatasetReader(image_path, density_path)
+    image_and_density_generator = date_reader.generate_image_and_density(size)
+    for i in range(1):
+        image, density_map = image_and_density_generator.__next__()
+        print(image)
+        print(density_map)
+    slice_and_label_generator = date_reader.generate_slice_and_labels(31, 4, size)
+    print("#" * 100)
+    for i in range(1):
+        slice_batch, label = slice_and_label_generator.__next__()
+        io.imshow(slice_batch[3])
+        print(slice_batch)
+        print(label)
