cbe15fb06777dc871751802264879cf0c79ebdd6
wangchenghan <869723801@qq.com>
2021-06-25 09:45:08 +0800

    update DatasetReader

--- DatasetReader.py
+++ DatasetReader.py
@@ -7,9 +7,9 @@ from skimage.transform import resize
 from utils import *
 
 class DatasetReader:
-    def __init__(self, image_path, density_path):
+    def __init__(self, image_path, density_map_path):
         self.image_path = image_path
-        self.density_path = density_path
+        self.density_map_path = density_map_path
 
         IMAGE_FORMAT = ['jpg', 'JPG', 'PNG', 'png', 'bmp', 'BMP']
 
@@ -19,14 +19,14 @@ class DatasetReader:
             for image_path in exists_images:
                 image_name = os.path.basename(image_path).split('.')[0]
                 label_name = image_name + '.npy'
-                density_path = os.path.join(self.density_path, label_name)
-                density_path = density_path if os.path.exists(density_path) else ''
-                self.file_path_list.append((image_path, density_path))
+                density_map_path = os.path.join(self.density_map_path, label_name)
+                density_map_path = density_map_path if os.path.exists(density_map_path) else ''
+                self.file_path_list.append((image_path, density_map_path))
 
     def generate_image_and_density(self, size=None):
-        for image_path, density_map in self.file_path_list:
+        for image_path, density_map_path in self.file_path_list:
             image = io.imread(image_path)
-            density_map = np.load(density_map) if density_map else None
+            density_map = np.load(density_map_path) if density_map_path else None
             if size and len(size) == 2:
                 image = img_as_ubyte(resize(image, size))
                 if not(density_map is None):
@@ -35,7 +35,7 @@ class DatasetReader:
                 raise Exception("图像的尺寸和密度图的尺寸不匹配，请重新生成，或在generate_slice_and_labels方法中指定一个新的尺寸（size）")
             yield image, density_map
 
-    def generate_slice_and_labels(self, window_size=31, batch_size=4, size=None):
+    def generate_slice_and_labels(self, window_size=31, batch_size=4, size=None, acclumate_area=False):
         image_batch = np.zeros((batch_size, window_size, window_size, 3), np.uint8)
         ground_truth_batch = np.zeros((batch_size), np.float)
         batch_index = 0
@@ -53,7 +53,10 @@ class DatasetReader:
                 for i in range(image.shape[0]):
                     for j in range(image.shape[1]):
                         cropped_image = np.array(padded_image[i: i + window_size, j: j + window_size])
-                        ground_truth_value = padded_density_map[(2 * i + window_size) // 2, (2 * j + window_size) // 2]
+                        if acclumate_area:
+                            ground_truth_value = np.sum(padded_density_map[i: i + window_size, j: j + window_size])
+                        else:
+                            ground_truth_value = padded_density_map[(2 * i + window_size) // 2, (2 * j + window_size) // 2]
                         image_batch[batch_index,:,:,:] = cropped_image
                         ground_truth_batch[batch_index] = ground_truth_value
                         batch_index += 1
@@ -77,9 +80,9 @@ class DatasetReader:
 
 if __name__ == '__main__':
     image_path = "/Users/Biomind/Documents/周文静/TasselNet/spruce/202011/train"
-    density_path = "/Users/Biomind/Documents/周文静/TasselNet/spruce/202011_den/train"
+    density_map_path = "/Users/Biomind/Documents/周文静/TasselNet/spruce/202011_den/train"
     size = (224, 224)
-    date_reader = DatasetReader(image_path, density_path)
+    date_reader = DatasetReader(image_path, density_map_path)
     image_and_density_generator = date_reader.generate_image_and_density(size)
     for i in range(1):
         image, density_map = image_and_density_generator.__next__()
@@ -90,5 +93,11 @@ if __name__ == '__main__':
     for i in range(1):
         slice_batch, label = slice_and_label_generator.__next__()
         io.imshow(slice_batch[3])
-        print(slice_batch)
+        # print(slice_batch)
+        print(label)
+    slice_and_label_generator = date_reader.generate_slice_and_labels(31, 4, size, True)
+    for i in range(1):
+        slice_batch, label = slice_and_label_generator.__next__()
+        io.imshow(slice_batch[3])
+        # print(slice_batch)
         print(label)
